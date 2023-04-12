ebdf8c7150913c76b25893f8e8cc217aeaec6680
muhammadalvin92 <alvinbomber87@gmail.com>
2023-04-12 16:49:25 +0700

    Footer & Perbaikan Navbar

---
+++ resources/views/part/footer.blade.php
@@ -0,0 +1,57 @@
+<style>
+    footer{
+        margin-top: 2.5rem;
+        background-color: #19376D;
+        padding: 5rem 0 2rem 0;
+    }
+    .text-justify{
+        text-align: justify;
+    }
+    i{
+        width: max-content;
+    }
+    hr {
+        margin: 2rem 0 .5rem 0;
+        color: inherit;
+        border: 0;
+        border-top: 1px solid;
+        opacity: .25;
+    }
+</style>
+<footer class="text-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+                <img src="{{asset('../img/logorpl.jfif')}}" width="100px" alt="">
+            </div>
+            <div class="col-md-4">
+                <h2>SIVALI FURNITURE</h2>
+                <p class="text-justify">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos nulla tenetur commodi! Dolor, eligendi fuga.</p>
+                <div class="sosmed d-flex gap-4 mt-3 mt-lg-4 mb-4 mb-lg-0">
+                    <i class="fa-brands fa-facebook-f fa-lg"></i>
+                    <i class="fa-brands fa-twitter fa-lg"></i>
+                    <i class="fa-brands fa-instagram fa-lg"></i>
+                    <i class="fa-brands fa-youtube fa-lg"></i>
+                    <i class="fa-brands fa-tiktok fa-lg"></i>
+                    <i class="fa-solid fa-circle-play fa-lg"></i>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <h2>MENU</h2>
+                <p class="mb-1">Home</p>
+                <p class="mb-1">Artikel</p>
+                <p class="mb-1">Katalog</p>
+                <p class="mb-1">Kontak Kami</p>
+            </div>
+            <div class="col-md-4">
+                <h2>ALAMAT</h2>
+                <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, delectus! Doloremque molestiae commodi quae rerum.</p>
+            </div>
+        </div>
+        <hr class="bg-white border-2 border-top border-white">
+        <div class="bottom_footer d-flex justify-content-between">
+            <p>@2020 Sivalin Furniture Jepara, All Rights Reserved</p>
+            <p>Terms & Condition - Privacy - Cookies</p>
+        </div>
+    </div>
+</footer>
--- resources/views/part/navbar.blade.php
+++ resources/views/part/navbar.blade.php
@@ -1,65 +1,23 @@
 <style>
-    *.icon-blue {
-        color: #0088cc;
-        }
-
-        *.icon-grey {
-        color: grey;
-        }
-
-        i {
-        width: 100px;
-        text-align: center;
-        vertical-align: middle;
-        position: relative;
-        }
-
-        .badge:after {
-        content: attr(data-count);
-        position: absolute;
-        background: #ff6600;
-        height: 2rem;
-        top: 1rem;
-        right: 1.5rem;
-        width: 2rem;
-        text-align: center;
-        line-height: 2rem;
-        font-size: 1rem;
-        border-radius: 50%;
-        color: white;
-        border: 1px solid #ff6600;
-        font-family: sans-serif;
-        font-weight: bold;
-    }
-
-    .badge {
-        padding-left: 9px;
-        padding-right: 9px;
-        -webkit-border-radius: 9px;
-        -moz-border-radius: 9px;
-        border-radius: 9px;
-        }
-
-        .label-warning[href],
-        .badge-warning[href] {
-        background-color: #c67605;
-    }
-
+   
     .navbar-collapse{
         flex-basis: 0;
         flex-grow: 0;
     }
+
+    nav{
+        z-index: 99;
+        background-color: white;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    }
 </style>
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg bg-light position-fixed w-100">
     <div class="container">
-        <div class="container-fluid d-flex justify-content-around align-items-center">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
             <div class="nav_logo">
                 <a class="navbar-brand" href="#"><img src="{{asset('../img/logorpl.jfif')}}" width="75px" alt=""></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
             </div>
-            <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarText">
+            <div class="collapse navbar-collapse" id="navbarText">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                   <a class="nav-link fs-5 active" aria-current="page" href="#">Home</a>
@@ -80,9 +38,11 @@
                     <i class="fa-lg fa-solid fa-magnifying-glass w-75"></i>
                     <i class="fa-lg fa-regular fa-user w-75"></i>
                     <i class="fa-lg fa-solid fa-cart-shopping w-75"></i>
-                    {{-- <i href="#" class="fa-solid fa-cart-shopping w-75"><asp:Label ID="lblCartCount" runat="server" CssClass="badge badge-warning"  ForeColor="White"/></i> --}}
                 </span>
             </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
         </div>
     </div>
-  </nav>
+</nav>
