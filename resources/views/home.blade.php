0edcdc33dc0b0765afc6790d71d6eb0d9f44ee2e
muhammadalvin92 <alvinbomber87@gmail.com>
2023-04-14 02:42:08 +0700

    Revisi Page Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,78 +1,19 @@
 @extends('layout.main')
-<style>
-    #top{
-        height: 100vh;
-    }
-    .circle{
-        top: -5rem
-    }
-    .circle1{
-        z-index: 1;
-        width: 20rem;
-        height: 20rem;
-        border-radius: 200px;
-        background: #19376D;
-    }
-    .circle2{
-        z-index: 1;
-        width: 5rem;
-        height: 5rem;
-        border-radius: 200px;
-        background: #19376D;
-    }
-    #ab_lemari{
-        /* margin-top: 1%; */
-        margin-left: 2%;
-        position: absolute;
-        z-index: 2;
-        width: 30%;
-    }
-    @media only screen and (max-width: 600px) {
-        #ab_lemari{
-        width: 100%;
-        }
-    }
+<link rel="stylesheet" href="{{asset('../css/home.css')}}">
 
-    .menu .menu_btn{
-        border: 1px solid rgb(187, 186, 186);
-        border-radius: 20px;
-    }
-    .menu .menu_btn:hover{
-        border: 1px solid rgb(187, 186, 186);
-        border-radius: 20px;
-        background: #19376D;
-        color: white;
-    }
-
-    .top_product{
-        /* background: red; */
-        width: 100%;
-        padding: 2.5rem;
-        height: 15rem;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    .top_product img{
-        width: 100%;
-    }
-    .rating .checked{
-        color: #ffc800;
-    }
-</style>
 @section('content')
     <div class="container d-flex align-items-center"  id="top">
        <div class="container-fluid">
         <div class="row">
             <div class="col-md-6 d-flex align-items-center">
                 <div class="desc">
-                    <h1>Sofa terbaru dengan kualitas terbaik</h1>
+                    <h1>Sofa terbaru dengan kualitas terbaik</h1> 
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet quis modi eius at quae fugit?</p>
                     <a class="btn btn-primary" href="#" role="button">Beli Sekarang</a>
                 </div>
             </div>
             <div class="col-md-6">
-                <img src="{{asset('../img/sofa.png')}}" width="110%" alt="">
+                <img src="{{asset('../img/sofa-home.png')}}" width="110%" alt="">
             </div>
         </div>
        </div>
@@ -81,34 +22,44 @@
     <div class="container-fluid mb-5">
         <div class="row d-flex justify-content-center text-center">
             <h3 class="mb-5">Kategori Barang</h3>
-            <div class="col-md-1 col-2">
-                <i class="fa-solid fa-coffee fa-2xl"></i>
-                <p class="mt-3">Kopi Hitam</p>
+            <div class="col-md-1 col-2 logo">
+                <div class="logo__img">
+                    <img src="{{asset('../img/bangku.png')}}" alt="">
+                </div>
+                <p class="mt-3">Bangku</p>
             </div>
-            <div class="col-md-1 col-2">
-                <i class="fa-solid fa-coffee fa-2xl"></i>
-                <p class="mt-3">Kopi Hitam</p>
+            <div class="col-md-1 col-2 logo">
+                <div class="logo__img">
+                    <img src="{{asset('../img/sofa-vector.png')}}" alt="">
+                </div>
+                <p class="mt-3">Sofa</p>
             </div>
-            <div class="col-md-1 col-2">
-                <i class="fa-solid fa-coffee fa-2xl"></i>
-                <p class="mt-3">Kopi Hitam</p>
+            <div class="col-md-1 col-2 logo">
+                <div class="logo__img">
+                    <img src="{{asset('../img/meja-vector.png')}}" alt="">
+                </div>
+                <p class="mt-3">Meja</p>
             </div>
-            <div class="col-md-1 col-2">
-                <i class="fa-solid fa-coffee fa-2xl"></i>
-                <p class="mt-3">Kopi Hitam</p>
+            <div class="col-md-1 col-2 logo">
+                <div class="logo__img">
+                    <img src="{{asset('../img/kasur-vector.png')}}" alt="">
+                </div>
+                <p class="mt-3">Kasur</p>
             </div>
-            <div class="col-md-1 col-2">
-                <i class="fa-solid fa-coffee fa-2xl"></i>
-                <p class="mt-3">Kopi Hitam</p>
+            <div class="col-md-1 col-2 logo">
+                <div class="logo__img">
+                    <img src="{{asset('../img/kamar-mandi-vector.png')}}" alt="">
+                </div>
+                <p class="mt-3">Kamar Mandi</p>
             </div>
         </div>
     </div>
 
-    <div class="d-flex align-items-center mb-4 pb-4" style="background: #E7E6E1; height: max-content; margin-top: 10rem;">
+    <div class="d-flex align-items-center mb-4 pb-4" style="background: #F5F6F8; height: max-content; margin-top: 10rem;">
         <div class="container">
             <div class="row gap-5">
                 <div class="col-md-5">
-                    <img id="ab_lemari" src="{{asset('../img/lemarireal.png')}}" alt="">
+                    <img id="ab_lemari" src="{{asset('../img/furniture-home.png')}}" alt="">
                     <div class="circle position-relative">
                         <div class="circle1"></div>
                         <div class="circle2"></div>
@@ -125,9 +76,9 @@
         </div>
     </div>
 
-    <div class="container" style="margin-top: 10rem;">
-        <h1 class="text-center">Katalog Kami</h1>
-        <div class="menu d-flex gap-2 flex-wrap justify-content-center mt-3">
+    <div class="container" style="margin-top: 7rem;">
+        <h1 class="text-center fw-bold">Katalog Kami</h1>
+        <div class="menu d-flex gap-2 flex-wrap justify-content-center mt-5">
             <a class="btn menu_btn" href="#" role="button">Semua</a>
             <a class="btn menu_btn" href="#" role="button">Terbaru</a>
             <a class="btn menu_btn" href="#" role="button">Terlaris</a>
