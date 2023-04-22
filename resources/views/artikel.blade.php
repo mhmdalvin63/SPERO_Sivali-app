6342d8dde2448a3ce48569fe6626f9f9bcafcf3c
muhammadalvin92 <alvinbomber87@gmail.com>
2023-04-22 21:01:03 +0700

    Responsive Page Artikel

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -9,8 +9,8 @@
     </div>
 </div>
 
-<div class="container mt-5">
-    <p class="title fw-bold fs-4 mb-0">Tipe Dapur Impian Anda Di Rumah</p>
+<div class="container" id="sec_content">
+    <p class="title fw-bold fs-3 mb-0">Tipe Dapur Impian Anda Di Rumah</p>
     <p class="desc_1 fs-5">Temukan semua barangnya hanya di Sivali Furniture    </p>
     <p class="desc_2">Lorem ipsum dolor sit amet consectetur. Sapien consectetur in quam suscipit ullamcorper ullamcorper. Viverra ac sodales fringilla facilisis at nulla eget ut. Nunc in posuere orci aenean mi tempus ut. Eget leo blandit erat urna varius urna. At hendrerit nunc fermentum accumsan mauris feugiat. Viverra nulla in enim vitae ac euismod vitae cras. Ipsum dictum tellus quis nulla et pharetra. Interdum praesent elit et sed ut est. Quis congue nisi elementum felis. Pellentesque urna in sed ultricies. Id eget consequat sed lectus id morbi.</p>
 </div>
@@ -23,9 +23,9 @@
                 <img class="img-fluid" src="{{asset('../img/image-artikel.png')}}" alt="">
                 <div class="overlay">
                     <p class="mb-1 fw-bold">Barang dengan kualitas Terbaik</p>
-                    <a href="" class="d-flex align-items-center gap-2 text-decoration-none text-white">
+                    <a href=""selengkapnya class="d-flex align-items-center gap-2 text-decoration-none text-white">
                         <i class="fa-solid fa-circle-chevron-right" style="color: #ffffff;"></i>
-                        <p class="mb-1">Selengkapnya</p>
+                        <p class="mb-0">Selengkapnya</p>
                     </a>
                 </div>
             </div>
@@ -33,9 +33,9 @@
                 <img class="img-fluid" src="{{asset('../img/image-artikel.png')}}" alt="">
                 <div class="overlay">
                     <p class="mb-1 fw-bold">Barang dengan kualitas Terbaik</p>
-                    <a href="" class="d-flex align-items-center gap-2 text-decoration-none text-white">
+                    <a href=""selengkapnya class="selangkapnya d-flex align-items-center gap-2 text-decoration-none text-white">
                         <i class="fa-solid fa-circle-chevron-right" style="color: #ffffff;"></i>
-                        <p class="mb-1">Selengkapnya</p>
+                        <p class="mb-0">Selengkapnya</p>
                     </a>
                 </div>
             </div>
@@ -43,9 +43,9 @@
                 <img class="img-fluid" src="{{asset('../img/image-artikel.png')}}" alt="">
                 <div class="overlay">
                     <p class="mb-1 fw-bold">Barang dengan kualitas Terbaik</p>
-                    <a href="" class="d-flex align-items-center gap-2 text-decoration-none text-white">
+                    <a href=""selengkapnya class="d-flex align-items-center gap-2 text-decoration-none text-white">
                         <i class="fa-solid fa-circle-chevron-right" style="color: #ffffff;"></i>
-                        <p class="mb-1">Selengkapnya</p>
+                        <p class="mb-0">Selengkapnya</p>
                     </a>
                 </div>
             </div>
