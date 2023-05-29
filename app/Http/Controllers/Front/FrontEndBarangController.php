7b73c0430eb38d237476345c0b5c3588d0346fc3
muhammadalvin92 <alvinbomber87@gmail.com>
2023-05-29 14:24:44 +0700

    Update Controller Katalog List Bottom

--- app/Http/Controllers/Front/FrontEndBarangController.php
+++ app/Http/Controllers/Front/FrontEndBarangController.php
@@ -38,6 +38,7 @@ class FrontEndBarangController extends Controller
     public function dataKategoriBarangKatalog(Request $request){
         $kategoriBarang = NewKategoriBarang::first()->get();
         $barang = NewBarang::with('KategoriBarang')->latest()->where('status','active')->paginate(4);
+        $allbarang = NewBarang::with('KategoriBarang')->latest()->get();
 
         $barangrandomkiri = NewBarang::orderByRaw('RAND()')->where('status','active')->get();
         $barangrandomkanan = NewBarang::orderByRaw('RAND()')->where('status','active')->get();
@@ -48,7 +49,7 @@ class FrontEndBarangController extends Controller
             // }
         // }
 
-        return view("katalog",compact('kategoriBarang','barang','barangrandomkiri','barangrandomkanan'));
+        return view("katalog",compact('kategoriBarang','barang','barangrandomkiri','barangrandomkanan','allbarang'));
     }
     public function dataKategoriBarangKatalogId($id){
         $KategoriBarangfilter = NewBarang::with('KategoriBarang')->where('id_kategori',$id)->where('status','active')->get();
