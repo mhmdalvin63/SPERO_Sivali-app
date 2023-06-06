bbd6621fedf8ba0adc72098128a50147b6370d2b
muhammadalvin92 <alvinbomber87@gmail.com>
2023-06-06 17:20:00 +0700

    Update Filtering No Refresh

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -20,6 +20,8 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 
 
+
+
     {{-- <link rel="stylesheet" href="{{ asset('../fontawesome/css/fontawesome.min.css') }}"/> --}}
     {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" /> --}}
     {{-- <style>
@@ -59,5 +61,8 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    
+        {{-- FILTERING   --}}
+        <script type="text/javascript" src="{{asset('../js/filtering.js')}}"></script>
   </body>
 </html>
