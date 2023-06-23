1a1a36de4d03b333222a49c4cef3350c9dd524fe
HafidBuroiroh <hafidburoiroh09@gmail.com>
2023-06-23 11:07:51 +0700

    fix bug

--- app/Http/Controllers/SigninController.php
+++ app/Http/Controllers/SigninController.php
@@ -2,42 +2,147 @@
 
 namespace App\Http\Controllers;
 
+use Mail;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class SigninController extends Controller
 {
-    public function login(){
-        return view('backEnd.login');
-       }
+    public function index()
+    {
+     return view('login');
+    }
+ 
+    public function adminlogin()
+    {
+     return view('backEnd.login');
+    }
+ 
+    public function register()
+    {
+     return view('register');
+    }
     
-       public function postlogin(Request $request){
-    
-            $input = $request->all();
-    
-            $this->validate($request, [
-                'email' => 'required|email|exists:users,email',
-                'password' => 'required|min:8',
-            ]);
-    
-            $credentials = array(
-                'email' => $input['email'],
-                'password' => $input['password'],
-            );
-    
-            if(Auth::attempt($credentials)){
-                if(auth()->user()->level == 'admin'){
-                    return redirect()->route('dashboard');
-                }else{
-                    return redirect()->route('signin');
-                }
-            }else{
-                return redirect()->route('signin');
-            }
-       }
-    
-       public function logout(){
-        Auth::logout();
-        return redirect()->route('signin');
-       }
-}
+    public function postregister(Request $request)
+    {
+     $this->validate($request, [
+         'name' => 'required',
+         'email' => 'required',
+         'password' => 'required|min:8'
+     ],[
+         'name' => 'Isi Nama User',
+         'email' => 'Isi email User',
+         'password' => 'Isi Password Minimal 8 Karakter'
+     ]);
+ 
+     $newUser = new User();
+     $newUser->name = $request->name;
+     $newUser->email =$request->email;
+     $newUser->password =  bcrypt($request->password);
+     $newUser->level = 'user';
+     $newUser->save();
+ 
+     return redirect('/login');
+    }
+ 
+ //    public function sendOtp($newUser)
+ //    {
+ //         $otp = rand(100000,999999);
+ //         $time = time();
+ 
+ //         EmailVerified::updateOrCreate(
+ //             ['email' => $newUser->email],
+ //             [
+ //                 'email' => $newUser->email,
+ //                 'otp' => $otp,
+ //             ]
+ //         );
+ 
+ //         $data['email'] = $newUser->email;
+ //         $data['title'] = 'Email Verification';
+ //         $data['body'] = 'Your OTP Verification is: '.$otp;
+ //         Mail::send('mailVerification',['data'=>$data],function($message) use ($data){
+ //             $message->to($data['email'])->subject($data['title']);
+ //         });
+ //    }
+ 
+ //    public function verification($id)
+ //    {
+ //         $user = User::find($id);
+ //         $email = $user->email;
+ 
+ //         $this->sendOtp($user);
+ 
+ //         return view('verification', compact('email'));
+ //    }
+ 
+ //    public function verified(Request $request)
+ //    {
+ 
+ //    }
+ 
+    public function authenticate(Request $request)
+    {
+         Session::flash('email', $request->email);
+         $this->validate($request, [
+             'email' => 'required|email|exists:users,email',
+             'password' => 'required|min:8',
+         ]);
+ 
+         $infologin = [ 
+             'email' => $request->email,
+             'password' => $request->password,
+         ];
+ 
+         if(Auth::attempt($infologin)){
+             if(auth()->user()->level == 'user'){
+             return redirect('/');
+             }
+             else{
+                 return redirect('/login');
+             }
+         }else{
+             return redirect('/login')->withErrors('Username atau Password Salah!');
+         }
+    }
+ 
+    public function postadmin(Request $request)
+    {
+         Session::flash('email', $request->email);
+         $this->validate($request, [
+             'email' => 'required|email|exists:users,email',
+             'password' => 'required|min:8',
+         ]);
+ 
+         $infologin = [ 
+             'email' => $request->email,
+             'password' => $request->password,
+         ];
+ 
+         if(Auth::attempt($infologin)){
+             if(auth()->user()->level == 'admin'){
+                 return redirect()->route('dashboard');
+             }
+             else{
+                 return redirect('/signin');
+             }
+         }else{
+             return redirect('/signin')->withErrors('Username atau Password Salah!');
+         }
+    }
+ 
+    public function logout()
+    {
+     Auth::logout();
+     return redirect('/');
+    }
+ 
+    public function logoutadmin()
+    {
+     Auth::logout();
+     return redirect('/');
+    }
+ }
+ 
